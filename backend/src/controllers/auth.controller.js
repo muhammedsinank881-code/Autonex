24510594cd582e6c18cc9a97b84f788a01de2f77
muhammedsinank881-code <muhammedsinank881@gmail.com>
2026-07-23 15:37:45 +0530

    Temporarily remove mongo sanitize

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -203,10 +203,12 @@ export const login = async (req, res) => {
 
     user.refreshToken = await bcrypt.hash(refreshToken, 10);
 
+    await user.save();
+
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
