913ee8811b9dfaa0ce6d936c88d353bd661a5501
muhammedsinank881-code <muhammedsinank881@gmail.com>
2026-07-24 14:45:48 +0530

    auth with resend otp

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -6,8 +6,12 @@ export const sendOTPEmail = async (email, otp) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
+
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
