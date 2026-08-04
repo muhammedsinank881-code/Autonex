0c5abbdb242822f9f929bdab58acc80d18845f16
muhammedsinank881-code <muhammedsinank881@gmail.com>
2026-08-04 18:37:50 +0530

    backend checkout API Implemented

--- backend/src/services/checkout.service.js
+++ backend/src/services/checkout.service.js
@@ -1,4 +1,4 @@
-import Cart from "../models/cart.model.js";
+import Cart from "../models/Cart.js";
 import Checkout from "../models/Checkout.js";
 
 const SHIPPING_CHARGE = Number(process.env.SHIPPING_CHARGE || 80);
